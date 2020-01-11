ed971820108a8441af1934c6296fefc3926d3dcd
Alex Elfering <anelfering@outlook.com>
2020-01-11 10:45:35 -0600

    Added documentation and removed pr section

--- Iowa Swim & Dive/Swim and Dive Script.R
+++ Iowa Swim & Dive/Swim and Dive Script.R
@@ -14,10 +14,12 @@ roster_full_name <- dplyr::mutate(roster_separate_names, Full.Name = paste(First
 roster_full_roster <- dplyr::select(roster_full_name, Season, Full.Name, Last.Name, Hometown, Class, Gender)
 
 ####  Swim & Dive Time Manipulation ####
-swim_no_relays <- dplyr::filter(swim_dive, !grepl("Relay|Diving",Event)) #  I want to focus solely on swimming and non-rela
+swim_no_relays <- dplyr::filter(swim_dive, !grepl("Relay|Diving",Event)) #  I want to focus solely on swimming and non-relay for now
 swim_no_relays$Time <- as.character(swim_no_relays$Time)
 swim_no_relays$Time_Count <- nchar(swim_no_relays$Time)
 
+# The time column is a mix of two, four, five, or more character column and I needed 
+# to manipulate the columns differently to get them into the same format.
 two_count <- swim_no_relays %>%
   filter(Time_Count <= 2) %>%
   mutate(Time = paste("0:", Time, ".0", sep = ""))
@@ -31,15 +33,10 @@ other_count <- swim_no_relays %>%
 
 swim_times <- bind_rows(two_count, four_five_count, other_count)
 
+# Time conversion to minutes
 swim_times$swim_decimal <- sapply(strsplit(swim_times$Time, ":"),
        function(x){
          x <- as.numeric(x)
          x[1] + x[2]/60
        }
 )
-
-pr_times <- swim_times %>%
-  group_by(Name, Event, Gender) %>%
-  slice(which.min(swim_decimal)) 
-
-write.csv(pr_times, file = 'swim_pr.csv')
