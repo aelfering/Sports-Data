ed1bcfbe742aa7707bc25e81c67ac8deb65d3ede
Alex Elfering <anelfering@outlook.com>
2020-01-28 11:29:30 -0600

    Cleaning up the data

--- College Soccer/College Soccer Cleaning Script.R
+++ College Soccer/College Soccer Cleaning Script.R
@@ -44,6 +44,7 @@ soccer_games_cleaned <- soccer_with_scores %>%
   ungroup() %>%
   as.data.frame()
 
+####  Plus-Minus Scores ####
 # Creating plus-minus scores
 plus_minus_soccer <- soccer_games_cleaned %>%
   group_by(Team, Gender) %>%
@@ -60,8 +61,32 @@ plus_minus_soccer <- soccer_games_cleaned %>%
   mutate(Plus.Minus.Rank = dense_rank(desc(Plus.Minus))) %>%
   ungroup()
 
-####  Estimating Overall Opponent Strength of Schedule ####
+####  Conference Play ####
+# Need more time with this section...please stay tuned!
+soccer_games_cleaned_conf <- left_join(soccer_games_cleaned, conferences, by = c('Team' = 'School', 'Gender' = 'Gender'))
+soccer_games_opp_conf <- left_join(soccer_games_cleaned_conf, conferences, by = c('Opponent.Name' = 'School', 'Gender' = 'Gender'))
 
+conf_rec <- soccer_games_opp_conf %>%
+  filter(Conference.x == Conference.y) %>%
+  group_by(Team, 
+           Conference.x, 
+           Gender) %>%
+  summarise(Conf.Wins = sum(Wins),
+            Conf.Losses = sum(Loses),
+            Conf.Ties = sum(Ties),
+            Conf.Goals.For = sum(Team.Score),
+            Conf.Goals.Against = sum(Opp.Score)) %>%
+  ungroup() %>%
+  mutate(Total.Games = Conf.Wins + Conf.Losses + Conf.Ties) %>%
+  group_by(Conference.x,
+           Gender) %>%
+  mutate(Pct.Conf = Conf.Wins/Total.Games,
+         Conf.Plus.Minus = Conf.Goals.For - Conf.Goals.Against) %>%
+  mutate(Conf.Rank = dense_rank(desc(Pct.Conf))) %>%
+  ungroup() %>%
+  as.data.frame()
+
+####  Estimating Overall Opponent Strength of Schedule ####
 opponent_record <- dplyr::select(plus_minus_soccer,
                                  Team,
                                  Gender,
@@ -90,14 +115,29 @@ distinct_matches <- inner_join(test_join1, distinct_games, by = c('Gender' = 'Ge
 
 sos_overall <- distinct_matches %>%
   filter(Result == 'W') %>%
-  group_by(Team, Gender) %>%
+  group_by(Team, 
+           Gender) %>%
   summarise(Opp.Wins = sum(Total.Wins),
             Opp.Losses = sum(Total.Losses),
             Opp.Ties = sum(Total.Ties),
             Opp.Goals.For = sum(Goals.For),
             Opp.Goals.Against = sum(Goals.Against)) %>%
   ungroup() %>%
-  as.data.frame()
+  mutate(Total.Games = Opp.Wins + Opp.Losses + Opp.Ties) %>%
+  mutate(Pct.Won = Opp.Wins/Total.Games) %>%
+  as.data.frame() %>%
+  select(Team,
+         Conference,
+         Gender,
+         Opp.Wins,
+         Opp.Losses,
+         Opp.Ties,
+         Opp.Goals.For,
+         Opp.Goals.Against,
+         Pct.Won) 
+
+college_soccer_data <- left_join(plus_minus_soccer, sos_overall, by = c('Gender' = 'Gender',
+                                                                        'Team' = 'Team'))
 
 ####  Exporting the dataset ####
 write.csv(plus_minus_soccer, file = 'college soccer plus minus.csv')
