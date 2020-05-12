8caa3bb02fa982f10ca2f626882139cf0c40a06c
Alex Elfering <alexelfering@Alexs-MacBook-Pro.local>
2020-05-12 09:41:51 -0500

    Added the intial script

---
+++ College Football/The History of Minnesota Football.R
@@ -0,0 +1,90 @@
+# The History of Minnesota Football by Coach
+# Small Multiples Visualization
+# Script By Alex Elfering
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+
+# Load the data set
+msp <- read.csv('minnesota fb history.csv')
+
+# Identify every season each coach led, even if they were interim
+coach_yrs <- msp %>%
+  distinct(Coach,
+           Season) %>%
+  group_by(Coach) %>%
+  mutate(Year_No = row_number(),
+         First_Season = as.character(min(Season)),
+         Latest_Season = as.character(max(Season))) %>%
+  ungroup() 
+
+# Calculate the rolling margin for every 11 games (modern teams play between 11-12 games each season)
+coach_roll <- msp %>%
+  select(Conference,
+         Coach,
+         Season,
+         Date,
+         Opponent,
+         Opp.Conference,
+         Result,
+         Points,
+         Opp.Points) %>%
+  group_by(Coach) %>%
+  mutate(Coach_Game_No = row_number(),
+         Games_Coached = n_distinct(Date),
+         Running.Margin = cumsum(Points - Opp.Points)) %>%
+  mutate(Wins = ifelse(Points > Opp.Points, 1, 0),
+         Loses = ifelse(Points < Opp.Points, 1, 0)) %>%
+  filter(Games_Coached >= 3) %>%
+  ungroup() %>%
+  mutate(Margin = Points - Opp.Points,
+         Rolling.Margin = rollapplyr(Margin, 11, sum, partial = TRUE),
+         Rollings.Wins = rollapplyr(Wins, 11, mean, partial = TRUE),
+         Coach_Name = Coach) 
+
+# Joining the dataframes together
+coach_df <- inner_join(coach_roll, coach_yrs, by = c('Coach' = 'Coach', 'Season' = 'Season'))
+
+# The visualization!
+# Using geom_line() and transform() to create a small multiples graph that highlights each coach by facet
+ggplot(coach_df, 
+       aes(x = Coach_Game_No, 
+           y = Rolling.Margin,
+           group = coach_order_name)) + 
+  geom_line(data = transform(coach_df,
+                             Coach = NULL), 
+            aes(group = paste(Coach_Name, as.character(Year_No), sep = '')), 
+            size = 1,
+            color = 'gray',
+            alpha = 0.7) +   
+  geom_hline(yintercept = 0,
+             color = '#898989',
+             linetype = 'dashed') +
+  geom_line(aes(group = paste(coach_order_name, as.character(Year_No), sep = '')), 
+            color = 'brown',
+            size = 1) + 
+  scale_colour_identity() + 
+  facet_wrap(~ paste(Coach, " (", First_Season, '-', Latest_Season, ")", sep ='')) +
+  labs(title = 'The History of Minnesota Football by Coach',
+       x = 'Game Number',
+       y = 'Rolling Margin',
+       subtitle = 'Rolling Plus-Minus Every 11 Games For Coaches Who Led at Least 3 Games',
+       caption = 'Since Minnesota joined the Western/Big Ten Conference.\nSource: College Football Reference\nVisualization by Alex Elfering') + 
+  theme(plot.title = element_text(face = 'bold', size = 18, family = 'Arial'),
+        plot.subtitle = element_text(size = 15, family = 'Arial'),
+        plot.caption = element_text(size = 12, family = 'Arial'),
+        axis.title = element_text(size = 12, family = 'Arial'),
+        axis.text = element_text(size = 12, family = 'Arial'),
+        strip.text = ggplot2::element_text(size = 12, hjust = 0, face = 'bold', color = 'brown', family = 'Arial'),
+        strip.background = element_rect(fill = NA),
+        panel.background = ggplot2::element_blank(),
+        axis.line = element_line(colour = "#222222", linetype = "solid"),
+        panel.grid.major.y = ggplot2::element_line(color = "#dedede", linetype = 'dashed'),
+        panel.grid.major.x = element_blank()) 
+
+
+
+
+
