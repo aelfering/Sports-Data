5d68b8a3621cb1182338256242ce2b52dfe78ab5
Alex Elfering <alexelfering@Alexs-MacBook-Pro.local>
2020-06-04 16:10:36 -0500

    Update Match Up History.R

--- College Football/Big Ten Head-to-Head Match Ups/Match Up History.R
+++ College Football/Big Ten Head-to-Head Match Ups/Match Up History.R
@@ -12,6 +12,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(tidyverse)
+library(cowplot)
 
 big_ten <- read.csv('history big ten.csv')
 
@@ -60,6 +61,56 @@ all_time_record <- big_ten %>%
                                          ifelse(Percent_Won <= 0.75, '50-75%', '75-100%'))))
 
 # The Visualization
+all_times_visual <- ggplot(all_time_record, 
+       aes(Opponent, 
+           School, 
+           fill = Percent_Buckets)) + 
+  # Establish Tiles
+  geom_tile(color = 'white',
+            size = 1,
+            aes(width = 0.95, 
+                height = 0.95)) +
+  # Set color for buckets
+  scale_fill_manual(values = c('#c6c6c6', '#4bacc5', '#1985a1', '#005f79')) +
+  # Titles and labels
+  labs(x = '',
+       y = '',
+       fill = 'Percent of Games Won') +
+  # Theme adjustments
+  theme(plot.title = element_text(face = 'bold', 
+                                  size = 18, 
+                                  family = 'Arial'),
+        legend.position = 'none',
+        axis.text.x = element_text(angle = 270, 
+                                   vjust = 0.5, 
+                                   hjust = 0, 
+                                   size = 10),
+        axis.title.y = element_text(angle = 270, 
+                                    hjust = 0.5, 
+                                    vjust = 0.5, 
+                                    size = 12),
+        axis.title.x = element_text(hjust = 0.5, 
+                                    vjust = 0.5, 
+                                    size = 12),
+        plot.subtitle = element_text(size = 15, 
+                                     family = 'Arial'),
+        plot.caption = element_text(size = 10, 
+                                    family = 'Arial'),
+        axis.title = element_text(size = 10, 
+                                  family = 'Arial'),
+        axis.text = element_text(size = 10, 
+                                 family = 'Arial'),
+        strip.text = ggplot2::element_text(size = 10, 
+                                           hjust = 0, 
+                                           face = 'bold', 
+                                           color = 'brown', 
+                                           family = 'Arial'),
+        strip.background = element_rect(fill = NA),
+        panel.background = ggplot2::element_blank(),
+        axis.line = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_blank()) 
+
 ggplot(all_time_record, 
        aes(Opponent, 
            School, 
@@ -72,8 +123,8 @@ ggplot(all_time_record,
   # Set color for buckets
   scale_fill_manual(values = c('#c6c6c6', '#4bacc5', '#1985a1', '#005f79')) +
   # Titles and labels
-  labs(x = '\n...Against Each Conference Opponent.',
-       y = '\nHow Each Team Performs...',
+  labs(x = '',
+       y = '',
        fill = 'Percent of Games Won') +
   # Theme adjustments
   theme(plot.title = element_text(face = 'bold', 
@@ -110,5 +161,16 @@ ggplot(all_time_record,
         panel.grid.major.y = element_blank(),
         panel.grid.major.x = element_blank()) 
 
-#print(mark1, vp = grid::viewport(width = 0.5, height=0.5, angle = -45))
+all_times_visual
+
+all_times_legend <- get_legend(all_times_visual)
+grid.newpage()
+grid.draw(all_times_legend)
+
+ggsave(
+  'new_visual3.png',
+  width = 5,
+  height = 5)
+
+
 
