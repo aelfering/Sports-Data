0703700bc4588166c0a9675014cadc103e184c76
Alex Elfering <alexelfering@Alexs-MacBook-Pro.local>
2020-07-22 06:28:23 -0500

    Updated comments at the top of the app

--- College Football/Conference Shiny App/ShinyAppTest/app.R
+++ College Football/Conference Shiny App/ShinyAppTest/app.R
@@ -1,11 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#   This shiny app compares metrics among conferences such as the win-loss-tie record, bowl record, total championships, and winningest team
+#   Created by Alex Elfering
+#   Updated 22 Jul 2020
 
 list.of.packages <- c("ggplot2", 
                       "dplyr", 
