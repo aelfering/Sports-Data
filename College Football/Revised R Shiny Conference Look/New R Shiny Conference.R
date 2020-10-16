28539fd766b77829b5723d7654eb905abec1a322
Alex Elfering <anelfering@outlook.com>
2020-10-16 14:39:11 -0500

    added dt

--- College Football/Revised R Shiny Conference Look/New R Shiny Conference.R
+++ College Football/Revised R Shiny Conference Look/New R Shiny Conference.R
@@ -12,10 +12,11 @@ library(data.table)
 library(zoo)
 library(ggrepel)
 library(rsconnect)
+library(DT)
 
 setwd("~/GitHub/Sports-Data/College Football/Revised R Shiny Conference Look")
 
-conf_performance <- read.csv('cfb conf.csv')
+conf_performance <- read.csv('cfb conf.csv' , fileEncoding="UTF-8-BOM")
 
 
 ui <- shinyUI(fluidPage(  
@@ -37,7 +38,8 @@ ui <- shinyUI(fluidPage(
                   value = 5),
       width=2),
     mainPanel(
-      plotOutput("Plot", width = "100%")  
+      plotOutput("Plot", width = "100%")  ,
+      dataTableOutput('dt')
     )  
   )  
 ))
@@ -130,8 +132,78 @@ server <- shinyServer(function(input, output) {
     
     
     
-  }, height=700,
-  width = 1150)
+  }, height=700, width = 1150)
+  output$dt <- renderDataTable({
+    
+    team_conf <- conf_performance %>%
+      replace(is.na(.), 0) %>%
+      arrange(Team, 
+              Season) %>%
+      select(Team, 
+             Season,
+             Conf,
+             Total.Wins,
+             Total.Losses,
+             Total.Ties,
+             Conf.Wins,
+             Conf.Losses,
+             Conf.Ties) %>%
+      mutate(Total.Games = Total.Wins + Total.Losses + Total.Ties,
+             Total.Conf.Games = Conf.Wins + Conf.Losses + Conf.Ties,
+             Pct.Win = Total.Wins/Total.Games) %>%
+      group_by(Team) %>%
+      mutate(Rolling.Wins = round(rollapplyr(Total.Wins, input$variable, input$running, partial = TRUE)),
+             Rolling.Losses = round(rollapplyr(Total.Losses, input$variable, input$running, partial = TRUE)),
+             Rolling.Ties = round(rollapplyr(Total.Ties, input$variable, input$running, partial = TRUE)),
+             Rolling.Total.Games = round(rollapplyr(Total.Games, input$variable, input$running, partial = TRUE))) %>%
+      ungroup() %>%
+      group_by(Team,
+               Conf) %>%
+      mutate(Rolling.Conf.Wins = round(rollapplyr(Conf.Wins, input$variable, input$running, partial = TRUE)),
+             Rolling.Conf.Losses = round(rollapplyr(Conf.Losses, input$variable, input$running, partial = TRUE)),
+             Rolling.Conf.Ties = round(rollapplyr(Conf.Ties, input$variable, input$running, partial = TRUE)),
+             Rolling.Conf.Total.Games = round(rollapplyr(Total.Conf.Games, input$variable, input$running, partial = TRUE))) %>%
+      ungroup() %>%
+      mutate(Rolling.Pct.Won = Rolling.Wins/Rolling.Total.Games,
+             Rolling.Conf.Pct.Won = Rolling.Conf.Wins/Rolling.Conf.Total.Games) %>%
+      filter(Season >= 1936)
+    
+    tbl_test <- team_conf %>%
+      filter(Conf == input$conference,
+             Season == input$season) %>%
+      arrange(desc(Rolling.Pct.Won)) %>%
+      unite(Record, c('Rolling.Wins', 'Rolling.Losses', 'Rolling.Ties'), sep = '-', na.rm = TRUE) %>%
+      unite(Latest.Record, c('Total.Wins', 'Total.Losses', 'Total.Ties'), sep = '-', na.rm = TRUE) %>%
+      unite(Conf.Record, c('Rolling.Conf.Wins', 'Rolling.Conf.Losses', 'Rolling.Conf.Ties'), sep = '-', na.rm = TRUE) %>%
+      unite(Latest.Conf.Record, c('Conf.Wins', 'Conf.Losses', 'Conf.Ties'), sep = '-', na.rm = TRUE) %>%
+      select(Team,
+             Record,
+             Latest.Record,
+             Conf.Record,
+             Latest.Conf.Record)
+    
+    sketch = htmltools::withTags(table(
+      class = 'display',
+      thead(
+        tr(
+          th(rowspan = 2, 'Team'),
+          th(colspan = 2, 'Overall'),
+          th(colspan = 2, 'Conference Play')
+        ),
+        tr(
+          lapply(rep(c(paste('Rolling ', tes_var, sep = ''), 'Last Season'), 2), th)
+        )
+      )
+    ))
+    
+    datatable(tbl_test, 
+              container = sketch, 
+              rownames = FALSE,
+              caption = paste(conf_var, ' Conference performance between ', season_var-rolling_var, ' and ', season_var, sep = ''),
+              options = list(paging = FALSE))
+    
+    
+  })
 })
 
 shinyApp(ui=ui, server=server)
