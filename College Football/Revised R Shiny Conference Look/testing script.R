2732863db4f00d5d62b29bc4789a0aab5b3cd1e0
Alex Elfering <anelfering@outlook.com>
2020-10-17 17:23:33 -0500

    Update testing script.R

--- College Football/Revised R Shiny Conference Look/testing script.R
+++ College Football/Revised R Shiny Conference Look/testing script.R
@@ -15,10 +15,12 @@ rolling_var <- 10
 
 tes_var <- 'mean'
 
-season_var <- 2014
-conf_var <- 'MAC'
+season_var <- 1980
+conf_var <- 'Ind'
 
 library(DT)
+
+head(conf_performance)
 
 team_conf <- conf_performance %>%
   replace(is.na(.), 0) %>%
@@ -27,6 +29,7 @@ team_conf <- conf_performance %>%
   select(Team, 
          Season,
          Conf,
+         Post,
          Total.Wins,
          Total.Losses,
          Total.Ties,
@@ -35,44 +38,59 @@ team_conf <- conf_performance %>%
          Conf.Ties) %>%
   mutate(Total.Games = Total.Wins + Total.Losses + Total.Ties,
          Total.Conf.Games = Conf.Wins + Conf.Losses + Conf.Ties,
-         Pct.Win = Total.Wins/Total.Games) %>%
+         Pct.Win = Total.Wins/Total.Games,
+         Finish.Ranked = ifelse(Post > 0, 1, 0),
+         Finished.Top.10 = ifelse(Post > 0 & Post <= 10, 1, 0)) %>%
   group_by(Team) %>%
+  mutate(Rolling.Wins = round(rollapplyr(Total.Wins, rolling_var, tes_var, partial = TRUE)),
+         Rolling.Losses = round(rollapplyr(Total.Losses, rolling_var, tes_var, partial = TRUE)),
+         Rolling.Ties = round(rollapplyr(Total.Ties, rolling_var, tes_var, partial = TRUE)),
+         Rolling.Total.Games = rollapplyr(Total.Games, rolling_var, tes_var, partial = TRUE),
+         Rolling.Ranked = rollapplyr(Finish.Ranked, rolling_var, sum, partial = TRUE),
+         Rolling.Top.10 = rollapplyr(Finished.Top.10, rolling_var, sum, partial = TRUE)) %>%
   ungroup() %>%
-  mutate(Rolling.Wins = rollapplyr(Total.Wins, rolling_var, tes_var, partial = TRUE),
-         Rolling.Losses = rollapplyr(Total.Losses, rolling_var, tes_var, partial = TRUE),
-         Rolling.Ties = rollapplyr(Total.Ties, rolling_var, tes_var, partial = TRUE),
-         Rolling.Total.Games = rollapplyr(Total.Games, rolling_var, tes_var, partial = TRUE)) %>%
   group_by(Team, Conf) %>%
-  mutate(Rolling.Conf.Wins = rollapplyr(Conf.Wins, rolling_var, tes_var, partial = TRUE),
-         Rolling.Conf.Losses = rollapplyr(Conf.Losses, rolling_var, tes_var, partial = TRUE),
-         Rolling.Conf.Ties = rollapplyr(Conf.Ties, rolling_var, tes_var, partial = TRUE),
+  mutate(Rolling.Conf.Wins = round(rollapplyr(Conf.Wins, rolling_var, tes_var, partial = TRUE)),
+         Rolling.Conf.Losses = round(rollapplyr(Conf.Losses, rolling_var, tes_var, partial = TRUE)),
+         Rolling.Conf.Ties = round(rollapplyr(Conf.Ties, rolling_var, tes_var, partial = TRUE)),
          Rolling.Conf.Total.Games = rollapplyr(Total.Conf.Games, rolling_var, tes_var, partial = TRUE)) %>%
   ungroup() %>%
   mutate(Rolling.Pct.Won = Rolling.Wins/Rolling.Total.Games,
          Rolling.Conf.Pct.Won = Rolling.Conf.Wins/Rolling.Conf.Total.Games) %>%
   filter(Season >= 1936)
 
-head(team_conf)
-
-team_conf %>%
-  filter(Conf == conf_var,
-         Season == season_var) %>%
-  select(Team,
-         Rolling.Wins) %>%
-  arrange(desc(Rolling.Wins)) #%>%
-  #filter(dense_rank(desc(Rolling.Wins)) <= input$ranking) %>%
-
 team_conf %>%
   filter(Conf == conf_var) %>%
-  group_by(Team) %>%
-  complete(Season = seq(min(Season), max(Season), by = 1)) %>%
+  group_by(Season) %>%
+  summarise(Rolling.Ranked = mean(Rolling.Ranked),
+            Rolling.Top.10 = mean(Rolling.Top.10)) %>%
   ungroup() %>%
-  fill(Conf) %>%
   ggplot(aes(x = Season,
-             y = Rolling.Wins,
-             group = Team)) +
-  geom_line() +
-  facet_wrap(~Team)
+             y = Rolling.Ranked)) +
+  geom_line()
+
+window <- 2
+team_conf %>%
+  filter(Conf == conf_var) %>%
+  rowwise() %>% 
+  mutate(Season = list(Season + lubridate::days(0:(window - 1)))) %>% 
+  unnest(cols = when) %>%
+  group_by(when) %>% 
+  summarise(twoDayCount = n_distinct(user))
 
 
+tbl_test <- team_conf %>%
+  filter(#Conf == conf_var,
+         Season == season_var) %>%
+  arrange(desc(Rolling.Pct.Won)) %>%
+  mutate(Rolling.Ties = ifelse(Rolling.Ties == 0, NA, Rolling.Ties),
+         Rolling.Conf.Ties = ifelse(Rolling.Conf.Ties == 0, NA, Rolling.Conf.Ties)) %>%
+  unite(Record, c('Rolling.Wins', 'Rolling.Losses', 'Rolling.Ties'), sep = '-', na.rm = TRUE) %>%
+  unite(Conf.Record, c('Rolling.Conf.Wins', 'Rolling.Conf.Losses', 'Rolling.Conf.Ties'), sep = '-', na.rm = TRUE) %>%
+  select(Team,
+         Record,
+         Conf.Record,
+         Rolling.Ranked,
+         Rolling.Top.10)
 
+tbl_test
