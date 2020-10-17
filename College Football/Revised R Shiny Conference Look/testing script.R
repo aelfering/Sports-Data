91485cbe5e6d37c32891c3298e0ba34a03f36b93
Alex Elfering <anelfering@outlook.com>
2020-10-17 08:39:30 -0500

    figured out the logic for line breaks

--- College Football/Revised R Shiny Conference Look/testing script.R
+++ College Football/Revised R Shiny Conference Look/testing script.R
@@ -11,11 +11,11 @@ setwd("~/GitHub/Sports-Data/College Football/Revised R Shiny Conference Look")
 
 conf_performance <- read.csv('cfb conf.csv' , fileEncoding="UTF-8-BOM")
 
-rolling_var <- 5
+rolling_var <- 10
 
 tes_var <- 'mean'
 
-season_var <- 1990
+season_var <- 2014
 conf_var <- 'MAC'
 
 library(DT)
@@ -52,64 +52,27 @@ team_conf <- conf_performance %>%
          Rolling.Conf.Pct.Won = Rolling.Conf.Wins/Rolling.Conf.Total.Games) %>%
   filter(Season >= 1936)
 
-top_teams <- team_conf %>%
+head(team_conf)
+
+team_conf %>%
   filter(Conf == conf_var,
          Season == season_var) %>%
   select(Team,
          Rolling.Wins) %>%
-  arrange(desc(Rolling.Wins)) %>%
-  filter(dense_rank(desc(Rolling.Wins)) <= 5) %>%
-  select(Team)
+  arrange(desc(Rolling.Wins)) #%>%
+  #filter(dense_rank(desc(Rolling.Wins)) <= input$ranking) %>%
 
-
-ggplot(team_conf,
-       aes(x = Season,
-           y = Rolling.Wins,
-           group = Team)) +
-  geom_line(alpha = 0.4,
-            color = 'gray') +
-  geom_line(data = subset(team_conf, Team %in% top_teams$Team),
-            mapping = aes(x = Season,
-                          y = Rolling.Wins,
-                          group = Team,
-                          color = Team),
-            size = 2,
-            alpha = 0.5) +
-  geom_line(data = subset(team_conf, Team %in% top_teams$Team & Season <= season_var),
-            mapping = aes(x = Season,
-                          y = Rolling.Wins,
-                          group = Team,
-                          color = Team),
-            size = 2,
-            alpha = 0.5) +
-  geom_vline(xintercept = season_var) +
-  theme(plot.title = element_text(face = 'bold', size = 18, family = 'Arial'),
-        legend.position = 'top',
-        legend.background=element_blank(),
-        legend.key=element_blank(),
-        legend.text = element_text(size = 12, family = 'Arial'),
-        legend.title = element_text(size = 12, family = 'Arial'),
-        plot.subtitle = element_text(size = 15, family = 'Arial'),
-        plot.caption = element_text(size = 12, family = 'Arial'),
-        axis.title = element_text(size = 12, family = 'Arial'),
-        axis.text = element_text(size = 12, family = 'Arial'),
-        strip.text = ggplot2::element_text(size = 12, hjust = 0, face = 'bold', color = 'black', family = 'Arial'),
-        strip.background = element_rect(fill = NA),
-        panel.background = ggplot2::element_blank(),
-        axis.line = element_line(colour = "#222222", linetype = "solid"),
-        panel.grid.major.y = element_line(colour = "#c1c1c1", linetype = "dashed"),
-        panel.grid.major.x = element_blank()) 
-
-
-
-
-
-
-
-
-
-
-
+team_conf %>%
+  filter(Conf == conf_var) %>%
+  group_by(Team) %>%
+  complete(Season = seq(min(Season), max(Season), by = 1)) %>%
+  ungroup() %>%
+  fill(Conf) %>%
+  ggplot(aes(x = Season,
+             y = Rolling.Wins,
+             group = Team)) +
+  geom_line() +
+  facet_wrap(~Team)
 
 
 
